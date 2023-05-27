9dff57951764c4cd86c7efc6f6f46ed79a0112f5
sphinxan <dazaj.osamu@mail.ru>
2023-05-27 18:01:33 +0500

    feat: add email send

---
+++ app/Mail/OrderShipping.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipping extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('example@example.com', 'Example')
+                ->markdown('emails.ordershipping');
+    }
+}
